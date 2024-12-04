c7a573cdcab24826ff7404fd099960b3e9d39f25
MatheusArnou <matheusbispo28@gmail.com>
2024-12-04 16:06:00 -0300

    Començando a trabalhar com conexões de API e classes HttpResponse/Request/Client

--- src/primeirosPassos/Desafios/conexoesHttp/ConsultCoin.java
+++ src/primeirosPassos/Desafios/conexoesHttp/ConsultCoin.java
@@ -1,4 +1,4 @@
-package br.com.alura.screenmatch.principal;
+package primeirosPassos.Desafios.conexoesHttp;
 
 import java.io.IOException;
 import java.net.URI;
@@ -7,20 +7,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class PrincipalComBusca {
+public class ConsultCoin {
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para busca: ");
-        var busca = leitura.nextLine();
+        System.out.println("Digite o nome da criptomoeda para a cotação (por exemplo, bitcoin): ");
+        var criptoNome = leitura.nextLine();
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=90d873fe";
+        String endereco = "https://api.coingecko.com/api/v3/simple/price?ids=" + criptoNome + "&vs_currencies=usd";
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
-
     }
 }
--- src/primeirosPassos/Desafios/conexoesHttp/ConsultaReceita.java
+++ src/primeirosPassos/Desafios/conexoesHttp/ConsultaReceita.java
@@ -1,4 +1,4 @@
-package br.com.alura.screenmatch.principal;
+package primeirosPassos.Desafios.conexoesHttp;
 
 import java.io.IOException;
 import java.net.URI;
@@ -7,20 +7,18 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.util.Scanner;
 
-public class PrincipalComBusca {
+public class ConsultaReceita {
     public static void main(String[] args) throws IOException, InterruptedException {
-
         Scanner leitura = new Scanner(System.in);
-        System.out.println("Digite um filme para busca: ");
-        var busca = leitura.nextLine();
+        System.out.println("Qual receita quer buscar (ex: bolo): ");
+        var receita = leitura.nextLine();
 
-        String endereco = "https://www.omdbapi.com/?t=" + busca + "&apikey=90d873fe";
+        String endereco = "https://www.themealdb.com/api/json/v1/1/search.php?s=" + receita;
+
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
-
         HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
-
     }
 }
